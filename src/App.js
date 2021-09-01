49788dd1a5bb22102bec60944eada6341e98367c
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2021-09-01 16:22:41 +0530

    basic application done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+
+import { jsPDF } from "jspdf";
 
 function App() {
+  const [fileUploaded, setFileUploaded] = useState(null);
+  const [fileName, setFileName] = useState("");
+
+  const handleFileUpload = async (e) => {
+    if (e.target.files[0]) {
+      setFileUploaded(await e.target.files[0].text());
+      setFileName(await e.target.files[0].name);
+    }
+  };
+  const saveDoc = () => {
+    const element = document.createElement("a");
+    let string = fileUploaded.replace("%PDF-1.3", "");
+    console.log(string);
+    const file = new Blob([string], {
+      type: "text/plain",
+    });
+    element.href = URL.createObjectURL(file);
+    element.download = `${fileName}`;
+    document.body.appendChild(element); // Required for this to work in FireFox
+    element.click();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="file" name="" id="" onChange={handleFileUpload} />
+
+      <button onClick={saveDoc}>Save</button>
     </div>
   );
 }

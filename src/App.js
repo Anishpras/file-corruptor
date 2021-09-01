e3a3aa308f30b1b18e0771616304ea512b379e13
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2021-09-01 17:33:15 +0530

    css done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import { useState } from "react";
 import "./App.css";
-
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 function App() {
   const [fileUploaded, setFileUploaded] = useState(null);
   const [fileName, setFileName] = useState("");
-
+  let date = new Date().getFullYear();
   const handleFileUpload = async (e) => {
     if (e.target.files[0]) {
       setFileUploaded(await e.target.files[0].text());
@@ -25,10 +25,59 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <input type="file" name="" id="" onChange={handleFileUpload} />
-
-      <button onClick={saveDoc}>Save</button>
+    <div className="w-screen h-screen flex flex-col  items-center bg-gray-200">
+      <div className="w-full h-20 bg-yellow-400 flex items-center justify-center shadow-2xl">
+        <h1 className="text-4xl text-white">PDF Corruptor free</h1>
+      </div>
+      <div className="w-full  items-center flex flex-col">
+        <div className="max-w-3xl mt-10 p-7">
+          <h2 className="text-3xl">
+            PDF File Corrupt - The PDF corrupter you were looking for!
+          </h2>
+          <h3 className="text-2xl mt-5">
+            Corrupt any PDF file with our free, online service.
+          </h3>
+          <p className="mt-3">
+            Homework submission the next day and you were fooling around. No
+            problem just give us your file and we will corrupt it and you will
+            get more time.
+          </p>
+          <h3 className="text-2xl mt-5">How to corrupt a file?</h3>
+          <p className="mt-3">
+            That's easy. Give us your file, we will damage it at a point you
+            can't imagine.
+          </p>
+          <p className="mt-3">This site can corrupt any kind of PDF file.</p>
+        </div>
+      </div>
+      <div className="bg-gray-50 flex flex-col justify-center items-center p-8 rounded-xl">
+        {" "}
+        <input type="file" name="" id="" onChange={handleFileUpload} />
+        {fileUploaded && (
+          <button
+            className="w-20 mt-4  p-2 rounded-lg border-green-300 border-2 hover:bg-green-400 hover:border-transparent"
+            onClick={saveDoc}>
+            Save
+          </button>
+        )}
+      </div>
+      <div className="bottom-0 bg-red-400 absolute w-screen flex justify-center items-center p-2">
+        <a
+          className="text-4xl mr-3"
+          href="https://github.com/Anishpras"
+          target="_blank"
+          rel="noopener noreferrer">
+          <FaGithub />
+        </a>
+        <a
+          className="text-4xl mr-3"
+          href="https://www.linkedin.com/in/anishpras118/"
+          target="_blank"
+          rel="noopener noreferrer">
+          <FaLinkedin />
+        </a>
+        <p>Copyright {date}</p>
+      </div>
     </div>
   );
 }
